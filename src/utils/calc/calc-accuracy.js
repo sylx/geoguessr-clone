35f50617275d473b49809dbd3f43e8620e7d330c
sylx <sharpoon@gmail.com>
2024-06-10 18:29:48 +0900

    Changed accuracy score formula

--- src/utils/calc/calc-accuracy.js
+++ src/utils/calc/calc-accuracy.js
@@ -1,5 +1,7 @@
 import naturalConfig from '../../config/natural.json';
 
-const calcAccuracy = (d,longest) => Math.max(1 - d / longest, 0);
+// y=\frac{\left(\left(1-\frac{x}{L}\right)^{a}+\left(1-\frac{x}{L}\right)\right)}{2}
+// a=70は適当な値
+const calcAccuracy = (x,L) => (Math.pow(1-x/L,70)+(1-x/L))/2;
 
 export default calcAccuracy;
